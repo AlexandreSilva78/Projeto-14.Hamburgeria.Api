915eab6dc10d087a23faa1dc1e828f02cda3cfe5
seu-username <alexandresilva.02101978@gmail.com>
2025-11-06 16:12:07 -0300

    Alterações no cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import fileRoutesConfig from './config/fileRoutes.cjs';
 import routes from './routes.js';
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/product-file', fileRoutesConfig);
 app.use('/category-file', fileRoutesConfig);
